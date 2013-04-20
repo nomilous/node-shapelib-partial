a0b3e8fd815044a4892c0f1c9784d19329f1bcbd
richardemslie <richard@clue.co.za>
2013-04-20 21:15:36 +0200

    callback into c binding

--- src/test.cc
+++ src/test.cc
@@ -11,12 +11,36 @@ Handle<Value> Square(const Arguments& args) {
 
   HandleScope scope;
 
-  Local<Number> num = Number::New(
+  //
+  // get callback from arg1
+  //
+
+  Local<Function> callback = Local<Function>::Cast(args[1]);
+
+  //
+  // calling back needs argCount and argArray
+  //
+
+  const unsigned argc = 1;
+  Local<Value> argv[argc] = Number::New(
+
+    //
+    // calculate squared value to call back
+    //
 
     args[0]->NumberValue() * args[0]->NumberValue()
 
   );
-  return scope.Close( num );
+  
+
+
+  callback->Call(
+
+    Context::GetCurrent()->Global(), argc, argv
+
+  );
+
+  return scope.Close( Undefined() );
 
 }
 
