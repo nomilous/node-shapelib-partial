a1b900455d8f4efaff4ff2b16bfbc5919ffd5fe2
richardemslie <richard@clue.co.za>
2013-04-20 21:05:13 +0200

    ignore build/Release

--- build/Release/.deps/Release/test.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/test.node := ./gyp-mac-tool flock ./Release/linker.lock g++ -shared -Wl,-search_paths_first -mmacosx-version-min=10.5 -arch x86_64 -L./Release -install_name @rpath/test.node  -o Release/test.node Release/obj.target/test/src/test.o -undefined dynamic_lookup
