442a8949619ed48e0343a8823e953885bae7efc2
richardemslie <richard@clue.co.za>
2013-04-20 21:02:23 +0200

    test

--- src/test.cc
+++ src/test.cc
@@ -1,5 +1,39 @@
-int foo(int bar) {
+#include <node.h>
+#include <v8.h>
 
-  return bar * bar;
+using namespace v8;
+
+//
+// define a function that squares the passed number
+// 
+
+Handle<Value> Square(const Arguments& args) {
+
+  HandleScope scope;
+
+  Local<Number> num = Number::New(
+
+    args[0]->NumberValue() * args[0]->NumberValue()
+
+  );
+  return scope.Close( num );
 
 }
+
+
+//
+// export the function
+// 
+
+void square(Handle<Object> exports) {
+
+  exports->Set(
+
+    String::NewSymbol("square"),
+    FunctionTemplate::New(Square)->GetFunction()
+
+  );
+
+}
+
+NODE_MODULE(test, square)
